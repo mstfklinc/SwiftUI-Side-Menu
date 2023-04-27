f990a06adb4e6e16a132cc44f7cc2135d439f3ce
Mustafa KILINÇ <mustafakilinc429@gmail.com>
2023-04-28 00:23:38 +0300

    -Created tabor

---
+++ SwiftUI-Side Menu/Model/Note.swift
@@ -0,0 +1,24 @@
+//
+//  Note.swift
+//  SwiftUI-Side Menu
+//
+//  Created by Mustafa Kılınç on 27.04.2023.
+//
+
+import SwiftUI
+
+struct Note: Identifiable {
+    var id = UUID()
+    var title: String
+    var subtitle: String
+    var caption: String
+    var color: Color
+    var image: Image
+}
+
+var notes = [
+    Note(title: "14 Essentials animations on SwiftUI", subtitle: "Build and animate an iOS app from scratch", caption: "Developing a good workflow", color: Color(hex: "7850F0"), image: Image("Topic 1")),
+    Note(title: "Build a Bottom navigation bar", subtitle: "Apply your Swift and SwiftUI knowledge by building real, quick and various applications from scratch", caption: "I wanted a curve animation", color: Color(hex: "6792FF"), image: Image("Topic 2")),
+    Note(title: "Anti-patterns of error handling in SwiftUI", subtitle: "This article will show common pitfalls you can make when handling exceptions and how to do it right", caption: "Article will help to avoid several popular...", color: Color(hex: "005FE7"), image: Image("Topic 1"))
+]
+
---
+++ SwiftUI-Side Menu/Styles/CustomButton.swift
@@ -0,0 +1,27 @@
+//
+//  CustomButton.swift
+//  SwiftUI-Side Menu
+//
+//  Created by Mustafa Kılınç on 28.04.2023.
+//
+
+import SwiftUI
+
+struct LargeButton: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(20)
+            .frame(maxWidth: .infinity)
+            .background(Color(hex: "F77D8E"))
+            .foregroundColor(.white)
+            .mask(RoundedCorner(radius: 20, corners: [.topRight, .bottomLeft, .bottomRight]))
+            .mask(RoundedRectangle(cornerRadius: 8))
+            .shadow(color: Color(hex: "F77D8E").opacity(0.5), radius: 20, x: 0, y: 10)
+    }
+}
+
+extension View {
+    func largeButton() -> some View {
+        modifier(LargeButton())
+    }
+}
